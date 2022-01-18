f1075822d2c2c5beb7c24f5e5442c7c9a76c4cf7
Gabriel Bunselmeyer <gabrielbunselmeyer@gmail.com>
2022-01-17 22:17:36 -0300

    Add configs

---
+++ server-data/pzrp_spawnregions.lua
@@ -0,0 +1,6 @@
+function SpawnRegions()
+        return {
+                { name = "Muldraugh, KY", file = "media/maps/Muldraugh, KY/spawnpoints.lua" },
+                { name = "Rosewood, KY", file = "media/maps/Rosewood, KY/spawnpoints.lua" },
+        }
+end
